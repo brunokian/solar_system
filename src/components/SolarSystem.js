6c252b46ea906fafabccee3dd2b12de00ed910a1
brunokian <brunokian@hotmail.com>
2022-05-10 01:00:16 -0300

    requisito 4

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Title from './Title';
 // import PropTypes from 'prop-types';
 
 class SolarSystem extends React.Component {
   render() {
-    return <div data-testid="solar-system">oi</div>;
+    return (
+      <div data-testid="solar-system">
+        <Title headline="Planetas" />
+      </div>
+    );
   }
 }
 
