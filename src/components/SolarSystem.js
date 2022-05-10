bc5c7b478c308b868d56d728af5e3268433b47ea
brunokian <brunokian@hotmail.com>
2022-05-10 00:33:07 -0300

    requisito 2

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class SolarSystem extends React.Component {
+  render() {
+    return <div data-testid="solar-system">oi</div>;
+  }
+}
+
+// SolarSystem.propTypes = {
+//   data: PropTypes.string,
+// };
+
+// SolarSystem.defaultProps = {
+//   data: undefined,
+// };
+
+export default SolarSystem;
